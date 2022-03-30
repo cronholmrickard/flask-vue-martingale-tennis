1a7a3b1316c96c9910e8ca31aa268a35a6fd4195
cronholmrickard <rickard.cronholm@spectronic.se>
2022-03-30 09:29:34 +0200

    Implemented sub class structure for all match related API Resources

--- backend/app/apis.py
+++ backend/app/apis.py
@@ -8,52 +8,61 @@ from flask_restful import Resource, abort, reqparse
 from app import db
 
 
-class Winner(Resource):
+class MatchData(Resource):
+    """Parent class for all Match related Resources"""
+
+    def __init__(self):
+        """constructor"""
+        self.collection = db.matches
+        super().__init__()
+
+
+class Winner(MatchData):
     """Get the winner of a match"""
 
-    def get(self):  # pylint: disable=no-self-use
+    def get(self):
         """get method"""
         parser = reqparse.RequestParser()
         parser.add_argument("id", type=str, required=True)
         pargs = parser.parse_args()
 
-        match = db.matches.find_one({"_id": pargs.id})
+        match = self.collection.find_one({"_id": pargs.id})
         if not match:
             abort(404, message=f"No match with id {pargs.id} in database")
         return jsonify(match[match["Winner"]])
 
 
-class Match(Resource):
+class Match(MatchData):
     """Defines the match resource"""
 
-    def get(self):  # pylint: disable=no-self-use
+    def get(self):
         """get method"""
         parser = reqparse.RequestParser()
         parser.add_argument("id", type=str, required=True)
         pargs = parser.parse_args()
 
-        match = db.matches.find_one({"_id": pargs.id})
+        match = self.collection.find_one({"_id": pargs.id})
         _ = match.pop("Winner")  # we don't want you sneek peaking at winners
         if not match:
             abort(404, message=f"No match with id {pargs.id} in database")
         return jsonify(match)
 
 
-class Matches(Resource):
+class Matches(MatchData):
     """Get indices of a number of matches"""
 
-    def get(self):  # pylint: disable=no-self-use
+    def get(self):
         """get method"""
         parser = reqparse.RequestParser()
         parser.add_argument("number", type=int, required=True)
         pargs = parser.parse_args()
-        _n_matches = db.matches.count_documents(filter={})
+        _n_matches = self.collection.count_documents(filter={})
         if pargs.number > _n_matches:
             abort(
                 406,
                 message=f"Too many mathces selected. Database contains {_n_matches} matches",
             )
-        data = list(db.matches.find())
+        data = list(self.collection.find())
         shuffle(data)
         return jsonify([x["_id"] for x in data[:pargs.number]])
 
